730fac0f4e0dd1a5b57e02ad3e57cfefc06f4838
HydroZA <jmslegge0@gmail.com>
2020-02-18 10:37:20 +0000

    Added Packet Library

---
+++ Packet/src/main/PacketFrame.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ *
+ * @author James Legge
+ */
+public class PacketFrame extends Packet<PacketFrame>
+{
+    private int size;
+    private byte[] frame;
+
+    public PacketFrame(byte[] frm, int size)
+    {
+        super(Packet.Type.FRAME);
+        this.frame = frm;
+        this.size = size;
+    }
+    
+    public PacketFrame()
+    {
+        super(Packet.Type.FRAME);
+    }
+    
+    public byte[] getFrame()
+    {
+        return frame;
+    }
+
+    public void setFrame(byte[] frame)
+    {
+        this.frame = frame;
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public void setSize(int size)
+    {
+        this.size = size;
+    }
+
+    @Override
+    public void serialize(DataOutputStream dos) throws IOException
+    {
+        dos.writeInt(size);
+        dos.write(frame);
+    }
+
+    @Override
+    public PacketFrame deserialize(DataInputStream dis) throws IOException
+    {
+        this.size = dis.readInt();
+        dis.readFully(frame, 0, size);
+        return this;
+    }
+
+}
---
+++ Packet/src/main/PacketFriends.java
@@ -0,0 +1,61 @@
+package main;
+
+/*
+    This class is used when the user first logs in and the friends are sent
+    to the client.
+
+    The PacketGetFriends class is used when the client requests friends after
+    they have already logged in.
+ */
+
+
+import java.io.*;
+
+public class PacketFriends extends Packet <PacketFriends>
+{
+    private String[] friends;
+
+    public PacketFriends(String[] friends)
+    {
+        super(Packet.Type.FRIENDS);
+        this.friends = friends;
+    }
+
+    public PacketFriends()
+    {
+        super(Packet.Type.FRIENDS);
+        this.friends = friends;
+    }
+
+    public String[] getFriends()
+    {
+        return friends;
+    }
+
+    public void setFriends(String[] friends)
+    {
+        this.friends = friends;
+    }
+
+    @Override
+    public void serialize(DataOutputStream dos) throws IOException
+    {
+        ObjectOutputStream os = new ObjectOutputStream(dos);
+        os.writeObject(this.friends);
+    }
+
+    @Override
+    public PacketFriends deserialize(DataInputStream dis) throws IOException
+    {
+        ObjectInputStream is = new ObjectInputStream(dis);
+        try
+        {
+            this.friends = (String[]) is.readObject();
+            return this;
+        }
+        catch (ClassNotFoundException e)
+        {
+            throw new IOException(e);
+        }        
+    }
+}
